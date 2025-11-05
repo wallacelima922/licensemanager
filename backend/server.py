add9abfd185b0c54da7cc6ecbc952d5c5289d8bf
wallacelima922 <wallace_lima@ymail.com>
2025-11-05 20:53:04 -0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -17,9 +17,9 @@ ROOT_DIR = Path(__file__).parent
 load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
-mongo_url = os.environ['MONGO_URL']
+mongo_url = os.environ.get('MONGO_URL')
 client = AsyncIOMotorClient(mongo_url)
-db = client[os.environ['DB_NAME']]
+db_name = os.environ.get('DB_NAME', 'license_system')
 
 # Security
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
